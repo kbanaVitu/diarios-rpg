92f823f03941f5fda88eefddf685af211d6aa7be
vitu <86437342+kbanaVitu@users.noreply.github.com>
2025-04-18 16:10:48 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 // Coloquei condição pq qnd tava testando apareceu q tava inicializando mais de uma vez, não descobri o motivo
 if (!firebase.apps.length) {
   const firebaseConfig = {
-    apiKey: "AIzaSyClCMg_cN9uftjqCLfKnvpFiKAANxjxTZY",
-    authDomain: "rpg-diaries.firebaseapp.com",
-    projectId: "rpg-diaries",
-    storageBucket: "rpg-diaries.firebasestorage.app",
-    messagingSenderId: "328444038132",
-    appId: "1:328444038132:web:0bb0106508dffb6fb587a1"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   // Inicializa o Firebase
