6255db81c00af6fd9206d675837f837b29da0040
vitu <86437342+kbanaVitu@users.noreply.github.com>
2025-05-08 22:21:34 -0300

    Delete src/entities/mestre/read.js

--- src/entities/mestre/read.js
+++
@@ -1,7 +0,0 @@
-window.getMestres = async function () {
-  const snapshot = await db.collection("mestres").orderBy("MestreId").get();
-  return snapshot.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data()
-  }));
-};
