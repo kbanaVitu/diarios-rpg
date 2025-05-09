d64525470d480de85ab6854ea7df8638fbf35e85
vitu <86437342+kbanaVitu@users.noreply.github.com>
2025-05-08 22:04:25 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyClCMg_cN9uftjqCLfKnvpFiKAANxjxTZY",
-  authDomain: "rpg-diaries.firebaseapp.com",
-  projectId: "rpg-diaries",
-  storageBucket: "rpg-diaries.firebasestorage.app",
-  messagingSenderId: "328444038132",
-  appId: "1:328444038132:web:0bb0106508dffb6fb587a1"
+  confidencial
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+window.firebaseConfig = firebaseConfig;
