18144c0cc42ccd5ea3fe98114b5c4736e797a76d
vitu <86437342+kbanaVitu@users.noreply.github.com>
2025-05-08 22:21:44 -0300

    Delete src/entities/mestre/update.js

--- src/entities/mestre/update.js
+++
@@ -1,7 +0,0 @@
-window.updateMestre = async function (docId, novoNome) {
-  if (!docId || !novoNome) throw new Error("ID e novo nome são obrigatórios");
-
-  await db.collection("mestres").doc(docId).update({
-    nome: novoNome.trim()
-  });
-};
